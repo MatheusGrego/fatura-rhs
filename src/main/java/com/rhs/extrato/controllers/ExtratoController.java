d433db24330d5cb01b9d5048a78eaae10d6296d6
MatheusGrego <matheusdeleongrego@gmail.com>
2023-06-26 14:06:10 -0300

    feat: Exception catching

--- src/main/java/com/rhs/extrato/controllers/ExtratoController.java
+++ src/main/java/com/rhs/extrato/controllers/ExtratoController.java
@@ -1,13 +1,17 @@
 package com.rhs.extrato.controllers;
 
 import com.rhs.extrato.services.ExtratoService;
+import org.apache.poi.EmptyFileException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/extrato")
@@ -17,7 +21,11 @@ public class ExtratoController {
 
     @PostMapping("/upload")
     public ResponseEntity<?> uploadExcel(@RequestPart("arquivo")MultipartFile file){
-        extratoService.readFile(file);
+        try {
+            extratoService.readFile(file);
+        }catch (IOException | EmptyFileException |MultipartException | NumberFormatException e){
+            return ResponseEntity.status(400).body("Error: "+ e.getMessage());
+        }
         return ResponseEntity.status(201).body("Dados salvos!");
 
     }
--- src/main/java/com/rhs/extrato/services/ExtratoService.java
+++ src/main/java/com/rhs/extrato/services/ExtratoService.java
@@ -3,37 +3,42 @@ package com.rhs.extrato.services;
 import com.rhs.extrato.models.Extrato;
 import com.rhs.extrato.repositories.ExtratoRepository;
 import com.rhs.extrato.util.DateUtil;
-import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.EmptyFileException;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.tika.Tika;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 import java.util.stream.IntStream;
 
 @Service
+@Slf4j
 public class ExtratoService {
     @Autowired
     private ExtratoRepository extratoRepository;
 
 
-    @SneakyThrows
-    public void readFile(MultipartFile file) {
+    public void readFile(MultipartFile file) throws IOException{
         if (file.isEmpty()) throw new EmptyFileException();
+        String mimeType  = new Tika().detect(file.getBytes());
+        if (!mimeType.equals("application/x-tika-ooxml"))
+            throw new MultipartException("Tipo de arquivo não permitido");
         fileIterator(file);
 
     }
 
-    @SneakyThrows
-    public void fileIterator(MultipartFile file) {
+    public void fileIterator(MultipartFile file) throws IOException {
         XSSFWorkbook workbook = new XSSFWorkbook(file.getInputStream());
         XSSFSheet sheet = workbook.getSheetAt(0);
 
@@ -47,30 +52,34 @@ public class ExtratoService {
                 .orElse(-1);
 
 
+        try {
+            if (startRow != -1) {
+                List<Extrato> dadosList = IntStream.range(startRow, sheet.getLastRowNum())
+                        .mapToObj(sheet::getRow)
+                        .filter(Objects::nonNull)
+                        .map(row -> {
+                            Extrato dados = Extrato.builder()
+                                    .id(UUID.randomUUID())
+                                    .documento(getStringValue(row, 1))
+                                    .login(getStringValue(row, 2))
+                                    .dataConsulta(DateUtil.convertExcelDate(getStringValue(row, 3)))
+                                    .horaConsulta(getStringValue(row, 4))
+                                    .valor(getStringValue(row, 5))
+                                    .ipConsulta(getStringValue(row, 6))
+                                    .nomeConsulta(getStringValue(row, 7))
+                                    .build();
 
-        if (startRow != -1) {
-            List<Extrato> dadosList = IntStream.range(startRow, sheet.getLastRowNum())
-                    .mapToObj(sheet::getRow)
-                    .filter(Objects::nonNull)
-                    .map(row -> {
-                        Extrato dados = Extrato.builder()
-                                .id(UUID.randomUUID())
-                                .documento(getStringValue(row, 1))
-                                .login(getStringValue(row, 2))
-                                .dataConsulta(DateUtil.convertExcelDate(getStringValue(row, 3)))
-                                .horaConsulta(getStringValue(row, 4))
-                                .valor(getStringValue(row, 5))
-                                .ipConsulta(getStringValue(row, 6))
-                                .nomeConsulta(getStringValue(row, 7))
-                                .build();
+                            return dados;
+                        })
+                        .toList();
 
-                        return dados;
-                    })
-                    .toList();
-
-            extratoRepository.saveAll(dadosList);
-            workbook.close();
+                extratoRepository.saveAll(dadosList);
+            }
+        } catch (NumberFormatException e) {
+            log.info("Planilha inválida!");
+            throw e;
         }
+        workbook.close();
 
 
     }
